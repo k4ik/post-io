9767301701d7bc2d5a4fc1345e886bba12ef7fe5
k4ik <kaikleal1234@gmail.com>
2024-06-07 20:18:58 -0300

    feat: create views

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<template>
+    <main>
+        <form id="form">
+            <fieldset>
+                <label for="email">Email:</label>
+                <input type="email" name="email">
+            </fieldset>
+            <fieldset>
+                <label for="password">Senha:</label>
+                <input type="password" name="password">
+            </fieldset>
+            <button @click.prevent="fetchData">Entrar</button>
+        </form>
+    </main>
+</template>
+
+<script>
+    export default {
+        methods: {
+            fetchData() {
+                let formData = new FormData(document.getElementByid("form"));
+
+                fetch("", {
+                    method: "POST",
+                    body: formData
+                })
+                .then(response => response.json())
+                .then(data => {
+
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+            }
+        },
+    }
+</script>
---
+++ frontend/src/views/SignupView.vue
@@ -0,0 +1,47 @@
+<template>
+    <main>
+        <form id="form">
+            <fieldset>
+                <label for="name">Nome:</label>
+                <input type="text" name="name">
+            </fieldset>
+            <fieldset>
+                <label for="email">Email:</label>
+                <input type="email" name="email">
+            </fieldset>
+            <fieldset>
+                <label for="password">Senha:</label>
+                <input type="password" name="password">
+            </fieldset>
+            <fieldset>
+                <label for="confirmPassword">Repita sua senha:</label>
+                <input type="password" name="confirmPassword">
+            </fieldset>
+            <button @click.prevent="fetchData">Cadastrar-se</button>
+        </form>
+    </main>
+    <!--<Mail />-->
+</template>
+
+<script>
+export default {
+    methods: {
+        fetchData() {
+            let form = document.getElementById("form");
+            let formData = new FormData(form);
+
+            fetch("", {
+                method: "POST",
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                // Lidar com a resposta
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
+    }
+}
+</script>
