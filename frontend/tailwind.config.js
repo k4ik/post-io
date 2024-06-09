fb12995170e05136fb33331ea418deba6d1c096c
k4ik <kaikleal1234@gmail.com>
2024-06-09 18:28:44 -0300

    feat: add custom configs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,30 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],  theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple': 'rgba(98, 82, 143, 1)',
+        'hpurple': 'rgba(72, 59, 109, 1)',
+        'form': 'rgba(27, 27, 27, 0.60)',        
+        'placeholder-color': 'rgba(238, 232, 232, 0.5)',
+        'text-color': '#EEE8E8',
+      },
+      height: {
+        '156': '39em'
+      },
+      borderRadius: {
+        '10': '40px',
+        '3': '12px'
+      },
+      fontFamily: {
+        'poppins': ["Poppins", "sans-serif"],
+        'jetbrains': ["JetBrains Mono", "monospace"],
+      },
+      inset: {
+        '7p': '7%',  
+        '19p': '19%',
+      },
+    },
   },
   plugins: [],
 }
